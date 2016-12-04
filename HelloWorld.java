26cf676585b5fb4c92e64f921508fe4e46118d40
Chris Lawrence <chris.lawrence@gmx.com>
2016-12-04 12:45:06 +0000

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld
+{
+  static String randomString = "Hello World";
+
+  static final double PINUM = 3.141529;
+
+  public static void main(String[] args)
+  {
+    int intgerOne = 22;
+    int intgerTwo = intgerOne + 1;
+
+    System.out.println(intgerTwo);
+
+  }
+}
